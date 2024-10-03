4f7d8be30035311f9c2e7add9d7e6890b858f9f4
Mustapha Amin <mustaphaamin2003@gmail.com>
2024-10-03 03:54:06 +0100

    db, provider, create task UI

--- lib/features/add_task/screens/add_task.dart
+++ lib/features/add_task/screens/add_task.dart
@@ -2,10 +2,12 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
-import 'package:task_mate/features/home/screens/home.dart';
+import 'package:task_mate/shared/spacing.dart';
 import 'package:task_mate/utils/colors.dart';
+import 'package:task_mate/utils/default_categories.dart';
 import 'package:task_mate/utils/extensions.dart';
 import 'package:task_mate/utils/textstyle.dart';
+import 'package:intl/intl.dart';
 
 class AddTask extends StatefulWidget {
   const AddTask({super.key});
@@ -17,7 +19,10 @@ class AddTask extends StatefulWidget {
 class _AddTaskState extends State<AddTask> {
   TextEditingController tasknameCtrl = TextEditingController();
   TextEditingController descriptionCtrl = TextEditingController();
-  TextEditingController dateCtrl = TextEditingController();
+  TextEditingController dateTimeCtrl = TextEditingController();
+  DateTime? date;
+  TimeOfDay? timeOfDay;
+  int categoryIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -29,87 +34,119 @@ class _AddTaskState extends State<AddTask> {
         ),
         centerTitle: true,
       ),
-      body: SizedBox(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Task Name",
-                    style: kTextStyle(25, isBold: true),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Task Name",
+                  style: kTextStyle(25, isBold: true),
+                ),
+                TextField(
+                  controller: tasknameCtrl,
+                  decoration: InputDecoration(
+                    hintText: "Task",
+                    hintStyle: kTextStyle(18, color: Colors.grey),
+                    border: OutlineInputBorder(),
                   ),
-                  TextField(
-                    controller: tasknameCtrl,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
+                ),
+                spaceY(20),
+                Text(
+                  "Category",
+                  style: kTextStyle(25, isBold: true),
+                ),
+                Wrap(
+                  spacing: 10,
+                  children: [
+                    ...taskCategories.map(
+                      (category) => ChoiceChip(
+                        selectedColor: AppColors.primaryColor,
+                        label: Text(
+                          category,
+                          style: kTextStyle(14,
+                              color: taskCategories.indexOf(category) ==
+                                      categoryIndex
+                                  ? Colors.white
+                                  : Colors.black),
+                        ),
+                        selected:
+                            taskCategories.indexOf(category) == categoryIndex,
+                        onSelected: (_) {
+                          setState(() {
+                            categoryIndex = taskCategories.indexOf(category);
+                          });
+                        },
+                      ),
                     ),
+                  ],
+                ),
+                spaceY(20),
+                Text(
+                  "Date and Time",
+                  style: kTextStyle(25, isBold: true),
+                ),
+                TextField(
+                  controller: dateTimeCtrl,
+                  decoration: InputDecoration(
+                    hintText: "Select date and time",
+                    hintStyle: kTextStyle(18, color: Colors.grey),
+                    suffixIcon: Icon(Icons.calendar_month),
+                    border: OutlineInputBorder(),
                   ),
-                  Text(
-                    "Category",
-                    style: kTextStyle(25, isBold: true),
-                  ),
-                  Text(
-                    "Date",
-                    style: kTextStyle(25, isBold: true),
-                  ),
-                  // TextField(
-                  //   controller: timeCtrl,
-                  //   enabled: true,
-                  //   readOnly: true,
-                  //   decoration: InputDecoration(
-                  //     border: OutlineInputBorder(),
-                  //     suffix: InkWell(
-                  //       onTap: () async {
-                  //         DateTime? pickedDate = await showDatePicker(
-                  //           context: context,
-                  //           firstDate: DateTime.now(),
-                  //           lastDate: DateTime(2099),
-                  //         );
-                  //         setState(() {
-                  //           dateCtrl.text = pickedDate.toString();
-                  //         });
-                  //       },
-                  //       child: Icon(
-                  //         Icons.calendar_month,
-                  //         color: Colors.blue,
-                  //       ),
-                  //     ),
-                  //   ),
-                  // ),
-                 
-                  Text("Description", style: kTextStyle(25, isBold: true)),
-                  TextField(
-                    controller: descriptionCtrl,
-                    maxLines: 5,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ],
+                  enabled: true,
+                  readOnly: true,
+                  onTap: () async {
+                    date = await showDatePicker(
+                      context: context,
+                      firstDate: DateTime.now(),
+                      lastDate: DateTime(2099),
+                    );
+                    if (date != null) {
+                      timeOfDay = await showTimePicker(
+                          // ignore: use_build_context_synchronously
+                          context: context,
+                          initialTime: TimeOfDay.now(),
+                          builder: (context, child) {
+                            return MediaQuery(
+                                data: MediaQuery.of(context)
+                                    .copyWith(alwaysUse24HourFormat: false),
+                                child: child!);
+                          });
+                      if (timeOfDay != null) {
+                        DateTime dateTime = DateTime(date!.year, date!.month,
+                            date!.day, timeOfDay!.hour, timeOfDay!.minute);
+                        setState(() {
+                          dateTimeCtrl.text =
+                              "${DateFormat('d/M/y').format(dateTime)} - ${DateFormat.Hm().format(dateTime)}";
+                        });
+                      }
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 50,
+            width: 100.w,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: AppColors.primaryColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+              onPressed: () {},
+              child: Text(
+                "Create task",
+                style: kTextStyle(20, color: Colors.white),
               ),
             ),
-            SizedBox(
-              height: 50,
-              width: 100.w,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: AppColors.primaryColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                ),
-                onPressed: () {},
-                child: Text(
-                  "Create task",
-                  style: kTextStyle(20, color: Colors.white),
-                ),
-              ),
-            ).padY(10),
-          ],
-        ),
+          ).padY(10),
+        ],
       ).padX(25),
     );
   }
---
+++ lib/shared/spacing.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+Widget spaceX(double size) {
+  return SizedBox(
+    width: size,
+  );
+}
+
+Widget spaceY(double size) {
+  return SizedBox(
+    height: size,
+  );
+}
